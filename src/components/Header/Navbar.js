d846a5e59860020e06bcb2aef53f74e331cf8225
Kushal Gera <kushalgera@Kushals-MacBook-Air.local>
2023-07-28 15:34:18 +0530

    Updated navbar

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 
 const Navbar = (props)=>{
@@ -12,6 +12,11 @@ const Navbar = (props)=>{
             newTitle += "Home"
         document.title = newTitle
     }, [props.category])
+
+    const location = useLocation()
+    useEffect(()=>{
+
+    }, [location])
 
     const scrollToDest = ()=>{
         var my_element = document.getElementById("body");
@@ -26,11 +31,11 @@ const Navbar = (props)=>{
         <div className="container">
             <div className="nav-scroller py-1 my-1">
                 <nav className="nav nav-underline justify-content-between">
-                    <Link onClick={scrollToDest} className="nav-item nav-link link-body-emphasis" to="/">Home</Link>
-                    <Link onClick={scrollToDest} className="nav-item nav-link link-body-emphasis" to="/technology">Technology</Link>
-                    <Link onClick={scrollToDest} className="nav-item nav-link link-body-emphasis" to="/business">Business</Link>
-                    <Link onClick={scrollToDest} className="nav-item nav-link link-body-emphasis" to="/politics">Politics</Link>
-                    <Link onClick={scrollToDest} className="nav-item nav-link link-body-emphasis" to="/health">Health</Link>
+                    <Link onClick={scrollToDest} className={`nav-item nav-link link-body-emphasis ${location.pathname==='/'?'active':''}`} to="/">Home</Link>
+                    <Link onClick={scrollToDest} className={`nav-item nav-link link-body-emphasis ${location.pathname==='/technology'?'active':''}`} to="/technology">Technology</Link>
+                    <Link onClick={scrollToDest} className={`nav-item nav-link link-body-emphasis ${location.pathname==='/business'?'active':''}`} to="/business">Business</Link>
+                    <Link onClick={scrollToDest} className={`nav-item nav-link link-body-emphasis ${location.pathname==='/politics'?'active':''}`} to="/politics">Politics</Link>
+                    <Link onClick={scrollToDest} className={`nav-item nav-link link-body-emphasis ${location.pathname==='/health'?'active':''}`} to="/health">Health</Link>
                     
                     <form className="d-flex mx-auto mx-md-0 mb-2 mb-md-0" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
